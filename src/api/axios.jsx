9c2b8bc882fa6c4296104e3b3f033081fe5f3eef
ndeyekoriadrame <dndeyekoria@gmail.com>
2025-10-06 18:11:54 +0000

    initial c

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://digimonnaie-backend-1.onrender.com",
+  baseURL: "https://digimonnaie-backend-1.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
